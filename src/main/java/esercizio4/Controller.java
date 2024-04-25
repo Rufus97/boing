59453b756401d88a777a660b74505dddf8b0dbfc
Rufus97 <manuelgomez@outlook.it>
2024-04-25 17:32:15 +0200

    add exercise

--- src/main/java/esercizio4/Controller.java
+++ src/main/java/esercizio4/Controller.java
@@ -1,6 +1,9 @@
 package esercizio4;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1")
 public class Controller {
-	
-	@GetMapping(path = "/prova")
-	public String prova(@RequestParam String nome) {
-		return ;
+
+	@PostMapping(path = "/reverse")
+	@CrossOrigin(origins = "http://localhost:8080")
+	public String reversatore(@RequestBody String name) {
+		StringBuilder reversata = new StringBuilder(name);
+		return reversata.reverse().toString();
+	}
+	@GetMapping(path = "/name")
+	@CrossOrigin(origins = "http://localhost:8080")
+	public String namer (@RequestParam String name) {
+		StringBuilder namer = new StringBuilder(name);
+		return namer.toString();
 	}
 
-	}
 }
