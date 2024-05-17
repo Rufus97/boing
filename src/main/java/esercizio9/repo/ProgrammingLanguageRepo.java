521cdeb14b24f3e50b54ef989c66a600f688aedd
Rufus97 <manuelgomez@outlook.it>
2024-05-17 23:14:34 +0200

    finish

---
+++ src/main/java/esercizio9/repo/ProgrammingLanguageRepo.java
@@ -0,0 +1,7 @@
+package esercizio9.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProgrammingLanguageRepo extends JpaRepository<ProgrammingLanguage, Long> {
+
+}
